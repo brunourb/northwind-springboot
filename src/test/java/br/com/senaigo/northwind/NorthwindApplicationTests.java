737a0219de245965894875258f5a7a926b369a83
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-05-10 22:13:46 -0300

    chore: inicio de projeto

---
+++ src/test/java/br/com/senaigo/northwind/NorthwindApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senaigo.northwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
